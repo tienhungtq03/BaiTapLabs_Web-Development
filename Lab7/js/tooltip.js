ac39e3f1b1ca23ceffc969a6edeb217a0f594685
nguyễn tiến hưng <nguyentienhungtq03@gmail.com>
2025-07-29 21:36:55 +0700

    Hoàn thành Lab 7

---
+++ Lab7/js/tooltip.js
@@ -0,0 +1,37 @@
+/* write functions that defines the action for each event */
+function showTip() {
+    var sidTip = document.getElementById("sidTip");
+    sidTip.style.display = "inline";
+}
+
+function hideTip() {
+    var sidTip = document.getElementById("sidTip");
+    sidTip.style.display = "none";
+}
+
+// Functions for password tooltip
+function showPwd1Tip() {
+    var pwd1Tip = document.getElementById("pwd1Tip");
+    pwd1Tip.style.display = "inline";
+}
+
+function hidePwd1Tip() {
+    var pwd1Tip = document.getElementById("pwd1Tip");
+    pwd1Tip.style.display = "none";
+}
+
+function init() {
+    var sid = document.getElementById("sid");
+    sid.onmouseover = showTip;
+    sid.onmouseout = hideTip;
+    sid.onfocus = showTip; /* for cursor on input field */
+    sid.onblur = hideTip; /* for cursor moving out */
+
+    var pwd1 = document.getElementById("pwd1"); // Get the password input
+    pwd1.onmouseover = showPwd1Tip;
+    pwd1.onmouseout = hidePwd1Tip;
+    pwd1.onfocus = showPwd1Tip;
+    pwd1.onblur = hidePwd1Tip;
+}
+
+window.onload = init;
